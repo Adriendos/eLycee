874e5f75f26ed1085c7f11782fe02d46a12077f3
Adriendos <dossantos.adrien18@gmail.com>
2014-11-25 11:53:59 +0100

    test

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    less: {
+	  development: {
+	    options: {
+	      paths: ["public/assets/css"]
+	    },
+	    files: {
+	      "public/assets/css/app.css": "public/assets/less/app.less"
+	    }
+	  }
+	}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('styles', ['less']);
+
+  grunt.registerTask('default', ['concat', 'uglify']);
+
+};
