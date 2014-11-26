e93f692e66d7304430e74ff054120149a9de73ac
de cuyper <decuyperjeremie@gmail.com>
2014-11-26 22:20:07 +0100

    test send put form via admin/index.html

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -64,16 +64,33 @@ class BaseController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function store($id)
+	public function store()
 	{
-		return $id;
 
-		// dd($id);
+		// $request = Route::getCurrentRoute()->getAction();
+		// $ctrl    = str_replace('Controller@store', '', $request['controller']);
 
-		$request = Route::getCurrentRoute()->getAction();
-		$ctrl    = str_replace('Controller@store', '', $request['controller']);
+		// $elem = new $ctrl();
 
-		$elem = new $ctrl();
+		// return 'testStore';
+
+		// $url = new Url;
+		// $url->url = Request::get('url');
+		// $url->description = Request::get('description');
+		// $url->user_id = Auth::user()->id;
+
+		// // La validation et le filtrage sont indispensables !!!
+		// // Vraiment, je suis impardonnable de laisser ça comme ça...
+
+		// $url->save();
+		var_dump($_POST);
+		die();
+
+		// return Response::json(array(
+		//   'error' => false,
+		//   'urls' => '$urls->toArray())',
+		//   200
+		// ));
 
         // $exp->save();
         // return Redirect::to('admin');
