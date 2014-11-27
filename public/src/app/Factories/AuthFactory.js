ade772139fd5ae0ed6321c828f1f0a1280914683
de cuyper <decuyperjeremie@gmail.com>
2014-11-28 00:26:10 +0100

    facto AuthFactory

---
+++ public/src/app/Factories/AuthFactory.js
@@ -0,0 +1,36 @@
+app.factory('auth', function($http) {
+
+  var userInfos,
+  urlBase = 'api/v1/auth',
+  auth    = {};
+
+  auth.login = function(userInfos) {
+    var request = {
+      method: 'POST', 
+      url: urlBase+'/login', 
+      data: userInfos 
+    };
+
+    return $http(request)
+    .success(
+      function(data, status, headers, config) {
+        // response = user obj
+      })
+    .error(
+      function(data, status, headers, config) {
+        console.log(headers);
+      });
+  };
+
+  // __ [TODO]
+  auth.logout = function() {
+
+  };
+
+  // __ [TODO]
+  auth.checkUser = function() {
+    
+  }
+
+  return auth;
+});
--- public/src/app/controllers/AuthController.js
+++ public/src/app/controllers/AuthController.js
@@ -1,35 +1,7 @@
-app.factory('auth', function($http) {
-
-  var userInfos,
-  urlBase = 'api/v1/auth',
-  auth    = {};
-
-  auth.login = function(userInfos) {
-    console.log(userInfos);
-    var request = {
-      method: 'POST', 
-      url: urlBase+'/login', 
-      data: userInfos 
-      // headers: { 'Content-Type' : 'application/x-www-form-urlencoded' }
-    };
-
-    return $http(request).success(
-      function(data, status, headers, config) {
-        console.log(data);
-      }).error(
-      function(data, status, headers, config) {
-        console.log(data);
-      });
-  };
-
-  return auth;
-});
-
 app.controller('ConnexionController', ['$scope', 'auth', function($scope, auth) {
 
   $scope.login = function() {
-
-    auth.login($scope.userInfos);z
+    auth.login($scope.userInfos);
   };
 
 }]);
