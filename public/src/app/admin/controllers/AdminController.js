3f21253c977e80a201c725eb1cb12d038a0957fb
Clement Peyrabere <clementpeyrabere@me.com>
2014-11-27 18:30:30 +0100

    fixed admin sidebar, todo: see why first click doesnt show sidebar

--- public/src/app/admin/controllers/AdminController.js
+++ public/src/app/admin/controllers/AdminController.js
@@ -1,6 +1,3 @@
 app.controller('AdminController',['$scope', function($scope) {
- 	$scope.showSidebar = function() {
- 		console.log('pigeon');
-		$('.left.vertical.sidebar').first().sidebar('attach events', '.launch.button', 'show');
-	};
+ 	
 }]);
