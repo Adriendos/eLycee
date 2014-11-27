165cada834ef6c48cc21e206b25058ca402097bb
de cuyper <decuyperjeremie@gmail.com>
2014-11-27 17:07:24 +0100

    factoring controller auth

--- public/src/app/app.js
+++ public/src/app/app.js
@@ -22,34 +22,4 @@ app.config(['$routeProvider',
     });
   }]);
 
-// ************* JEREMIE => Authenticate user url laravel = api/v1/auth/login api/v1/auth/logout
-app.factory('auth', function($http) {
-
-  var userInfos,
-  urlBase = 'api/v1/auth',
-  auth    = {};
-
-  auth.login = function(userInfos) {
-
-    $http.post(urlBase+'/login', userInfos).
-      success(function(data, status, headers, config) {
-        console.log(data);
-      }).
-      error(function(data, status, headers, config) {
-        console.log(data);
-      });
-  };
-
-  return auth;
-});
-
-app.controller('ConnexionController', ['$scope', 'auth', function($scope, auth) {
-
-  $scope.login = function() {
-    auth.login($scope.user);
-  };
-
-}]);
-
-
 
