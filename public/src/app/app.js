b81ceecbe35f07440c82d85dcde7fa57b0e704b8
de cuyper <decuyperjeremie@gmail.com>
2014-11-27 16:01:28 +0100

    test factory

--- public/src/app/app.js
+++ public/src/app/app.js
@@ -58,40 +58,72 @@ app.controller('ContactController', ['$scope', function($scope) {
 
 
 // ************* JEREMIE => Authenticate user url laravel = api/v1/auth/login api/v1/auth/logout
-app.factory('auth', function($http, $q, $window) {
-  var userInfos;
+// app.factory('auth', function($http, $q, $window) {
+//   var userInfos;
  
-  function login(userName, password) {
-    var deferred = $q.defer();
+//   function login(userName, password) {
+//     var deferred = $q.defer();
  
-    $http.post('/api/login', {
-      userName: userName,
-      password: password
-    }).then(function(result) {
-      userInfo = {
-        accessToken: result.data.access_token,
-        userName: result.data.userName
-      };
-      $window.sessionStorage['userInfos'] = JSON.stringify(userInfo);
-      deferred.resolve(userInfo);
-    }, function(error) {
-      deferred.reject(error);
-    });
+//     $http.post('/api/login', {
+//       userName: userName,
+//       password: password
+//     }).then(function(result) {
+//       userInfo = {
+//         accessToken: result.data.access_token,
+//         userName: result.data.userName
+//       };
+//       $window.sessionStorage['userInfos'] = JSON.stringify(userInfo);
+//       deferred.resolve(userInfo);
+//     }, function(error) {
+//       deferred.reject(error);
+//     });
  
-    return deferred.promise;
-  }
+//     return deferred.promise;
+//   }
  
-  return {
-    login: login
-  };
-});
+//   return {
+//     login: login
+//   };
+// });
 
-app.controller('ConnexionController', ['$scope', 'auth', function($scope, auth) {
+app.factory('dataFactory', ['$http', function($http) {
+
+    var urlBase = '/api/customers';
+    var dataFactory = {};
+
+    dataFactory.getCustomers = function () {
+        return $http.get(urlBase);
+    };
+
+    dataFactory.getCustomer = function (id) {
+        return $http.get(urlBase + '/' + id);
+    };
+
+    dataFactory.insertCustomer = function (cust) {
+        return $http.post(urlBase, cust);
+    };
+
+    dataFactory.updateCustomer = function (cust) {
+        return $http.put(urlBase + '/' + cust.ID, cust)
+    };
+
+    dataFactory.deleteCustomer = function (id) {
+        return $http.delete(urlBase + '/' + id);
+    };
+
+    dataFactory.getOrders = function (id) {
+        return $http.get(urlBase + '/' + id + '/orders');
+    };
+
+    return dataFactory;
+}]);
+
+app.controller('ConnexionController', ['$scope', 'auth', 'userFactory', function($scope, auth) {
 
   $scope.userConnection = function() {
     console.log($scope.user);
   };
-  
+
 }]);
 
 
