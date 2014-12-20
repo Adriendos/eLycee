06b667394de2959d27855deb9f2bd8f0f89aa93f
de cuyper <decuyperjeremie@gmail.com>
2014-12-20 16:13:36 +0100

    commit fixed

--- public/js/factories/PostsFactory.js
+++ public/js/factories/PostsFactory.js
@@ -3,14 +3,9 @@ app.factory('postsFactory', ['$http', '$resource', function($http, $resource) {
         "api/v1/posts/:id",
         {id: "@id" },
         {
-<<<<<<< HEAD
-            "update": {method: "PUT"},
-            "reviews": {'method': 'GET', 'params': {'reviews_only': "true"}}
-=======
-           query: {method: 'GET', isArray: true},
-           get: {method: 'GET', params:{id:'@id'}, isArray: true},
-           save: {method: 'POST', isArray: true}
->>>>>>> 775be06ee7f6c6c52bc0e4fee25755adeac06f4d
+          query: {method: 'GET', isArray: true},
+          get: {method: 'GET', params:{id:'@id'}, isArray: true},
+          save: {method: 'POST', isArray: true}
         }
     );
 }]);
