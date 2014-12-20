af458882b131b2d78c9b28aa2e59f34d236a8014
Clement Peyrabere <clementpeyrabere@me.com>
2014-12-20 14:12:22 +0100

    ngResource for api rest calls

--- public/js/admin/controllers/AdminController.js
+++ public/js/admin/controllers/AdminController.js
@@ -1,3 +1,3 @@
 app.controller('AdminController',['$scope', function($scope) {
- 	
+	 	
 }]);
--- public/js/controllers/NewsController.js
+++ public/js/controllers/NewsController.js
@@ -1,11 +1,17 @@
 app.controller('NewsController', ['$http', '$location', '$scope', function($http, $location, $scope) {
-  $http.get('api/v1/posts').
-    success(function(data, status, headers, config) {
-      $scope.allPosts = data;
-      console.log(data);
-    }).
-    error(function(data, status, headers, config) {
-      $scope.notify('La requête vers le serveur a échoué... Réessayez.', 'error');
-      console.log(config);
-    });
+  $scope.posts;
+
+    getAllPosts();
+
+    function getAllPosts() {
+      postsFactory.query().success(function (posts) {
+            $scope.posts = posts;
+        })
+        .error(function (error) {   
+            $scope.notify('La requête vers le serveur a échoué... Réessayez.','error');
+            $scope.status = 'Unable to load post data: ' + error.message;
+        });;
+      postsFactory.getAllPosts()
+        
+    }
 }]);
