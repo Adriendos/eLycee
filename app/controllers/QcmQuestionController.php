bf9824e48ed19afff8621f4bff0f5127a7a99e67
Clement Peyrabere <clementpeyrabere@me.com>
2015-01-06 19:22:17 +0100

    pb

--- app/controllers/QcmQuestionController.php
+++ app/controllers/QcmQuestionController.php
@@ -1,5 +1,56 @@
 <?php 
 
 class QcmQuestionController extends \NestedBaseController {
-	
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return json element created
+	 */
+	public function store()
+	{
+    }
+
+	 /**
+	 * Display the specified resource.
+	 *
+	 * @param  int $id
+	 * @return json element to display
+	 */
+	public function show($firstResourceId, $secondResourceId)
+	{
+
+	}
+
+	/**
+	 * Update the specified resource from storage
+	 *
+	 * @param int $id
+	 * @return json element updated
+	 */
+	public function update($id)
+	{
+
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return str delete message
+	 */
+	public function destroy($id)
+	{
+
+	}
 }
