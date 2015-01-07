98e18a712b225e14d7473f830dd99fcf2ec3aad3
Clement Peyrabere <clementpeyrabere@me.com>
2015-01-07 16:07:55 +0100

    view student qcm okkkkk

--- public/js/controllers/QcmCtrl.js
+++ public/js/controllers/QcmCtrl.js
@@ -1,5 +1,34 @@
-app.controller('QcmCtrl',['$scope', function($scope) {
+app.controller('QcmCtrl',['$scope', 'ENTITY', 'DataAccess', 'SessionService',
+    function($scope, ENTITY, DataAccess, SessionService) {
     var self = this;
+    var allQcms = {};
+    var allScores = {};
+
+    DataAccess.getAllData(ENTITY.qcm).then(
+        function(qcms) {
+            allQcms = qcms;
+            var classQcms = _.filter(allQcms, function(item){ return item.class_level == SessionService.getUser().role });
+            $scope.availableQcms = [];
+            $scope.unavailableQcms = [];
+            DataAccess.getAllData(ENTITY.score).then(
+                function(scores) {
+                    allScores = scores;
+                    angular.forEach(classQcms, function(qcm) {
+                        var score =_.find(scores, function(score){ return score.user_id == SessionService.getUser().id && score.qcm_id == qcm.id ;});
+                        if(score) {
+                            //unavailable qcm
+                            qcm.score = score.score;
+                            $scope.unavailableQcms.push(qcm);
+                        } else {
+                            //available qcm
+                            $scope.availableQcms.push(qcm);
+                        }
+                    });
+                }
+            );
+
+        }
+    );
 
     $scope.chartObject = {
         "type": "PieChart",
