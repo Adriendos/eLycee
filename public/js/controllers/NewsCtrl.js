da2aef168d4e224971fb2e2d01ae00317aaf730b
de cuyper <decuyperjeremie@gmail.com>
2015-01-10 00:42:12 +0100

    ok date

--- public/js/controllers/NewsCtrl.js
+++ public/js/controllers/NewsCtrl.js
@@ -11,6 +11,7 @@ function($scope, ENTITY, DataAccess) {
             $scope.allPosts = _.filter(posts, function(post) { return post.status == 'published'});
             $scope.posts = DataAccess.getPage($scope.allPosts, 1);
             $scope.nbPages = DataAccess.getNbPage(posts);
+            
             // console.info('date', new Date($scope.posts[0].created_at).getTime());
             // console.info('type date', typeof new Date($scope.posts[0].created_at).toISOString());
         }
