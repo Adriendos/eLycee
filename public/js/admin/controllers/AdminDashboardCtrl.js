a2a6fe5c359ad99d5aa9e5bf6e2024f2aa22f071
de cuyper <decuyperjeremie@gmail.com>
2015-01-10 21:39:31 +0100

    ok

--- public/js/admin/controllers/AdminDashboardCtrl.js
+++ public/js/admin/controllers/AdminDashboardCtrl.js
@@ -1,46 +1,43 @@
 app.controller('AdminDashboardCtrl',
-    ['$rootScope', '$scope', 'DataAccess', 'ENTITY', '$filter',
-        function($rootScope, $scope, DataAccess, ENTITY, $filter) {
-        	
-            /**
-             * init vars
-             **/
-            var resources = [ // app resources
-                ENTITY.user, ENTITY.post, ENTITY.qcm, ENTITY.score
-            ];
+   ['$rootScope', '$scope', 'DataAccess', 'ENTITY', '$filter',
+       function($rootScope, $scope, DataAccess, ENTITY, $filter) {
+           
+           /**
+            * init vars
+            **/
+           var resources = [ // app resources
+               ENTITY.user, ENTITY.post, ENTITY.qcm, ENTITY.score
+           ];
 
-            $scope.newsFeed = [];
+           $scope.newsFeed = [];
 
-            for (var i = 0; i < resources.length; i++) {
-                _getDatasAndSetNumber(resources[i]);
-            };
+           for (var i = 0; i < resources.length; i++) {
+               _getDatasAndSetNumber(resources[i]);
+           };
 
-            /**
-             * private methods
-             **/
-            function _getDatasAndSetNumber(resourceName) { // set datas dynamicly
-                DataAccess.getAllData(resourceName).then( function(data) {
-                    $scope[resourceName] = data;
+           /**
+            * private methods
+            **/
+           var newsFeed;
+           function _getDatasAndSetNumber(resourceName) { // set datas dynamicly
+               DataAccess.getAllData(resourceName).then( function(data) {
+                   $scope[resourceName] = data;
 
-                    var nameNumber = resourceName + 'Number';
-                    $scope[nameNumber] = Object.keys(data).length - 2; // prop promises
-                    if(resourceName == 'users') return;
-                    var tenFirsts = _.first(data, 10);
-                    $scope.newsFeed = _.union($scope.newsFeed, tenFirsts);
-                    $scope.newsFeed.sort(function (a, b) {
-                        //Parsing timeStamps to dates
-                        var t = a.updated_at.split(/[- :]/);
-                        var t2 = b.updated_at.split(/[- :]/);
-                        var d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
-                        console.log(d);
+                   var nameNumber = resourceName + 'Number';
+                   $scope[nameNumber] = Object.keys(data).length - 2; // prop promises
+                   if(resourceName == 'users') return;
+                   var tenFirsts = _.first(data, 10);
+                   newsFeed = _.union($scope.newsFeed, tenFirsts);
+                   newsFeed.sort(function (a, b) {
+                       //Parsing timeStamps to dates
+                       if (new Date(a.updated_at) > new Date(b.updated_at)) return -1;
+                       if (new Date(a.updated_at) < new Date(b.updated_at)) return 1;
+                       return 0;
+                   });
+                   $scope.newsFeed = newsFeed;
+               });
+           };
 
-                        var d2 = new Date(t2[0], t2[1]-1, t2[2], t2[3], t2[4], t2[5]);
-                        console.log(d2);
 
-                        if (d < d2) return 1;
-                        if (d2 < d) return -1;
-                        return 0;
-                    });
-                });
-            };
-        }]);
+
+       }]);
