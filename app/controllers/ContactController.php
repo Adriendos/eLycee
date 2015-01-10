f87981b0e64da7868fe20a0a62c037f54d6efab4
de cuyper <decuyperjeremie@gmail.com>
2015-01-10 18:12:14 +0100

    init errors form contact

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -4,40 +4,15 @@ class ContactController extends Controller {
 	public function sendContactMail() 
 	{
 		$inputs = Input::All();
-		extract($inputs);
-		// envoi au webmaster
-		
-		/**
-		 * @params 1st -> body class in the mail
-		 * 		   2nd -> data to be passed  
-		 * 		   3rd -> function
-		 **/
-// 		Mail::send('email.contact',$inputs, function($m)
-// 		{
-// 		    //$m->from($mail, $nom." ".$prenom);
-
-// 		    $m->to(['dossantos.adrien18@gmail.com', 'clementpeyrabere@gmail.com', 'decuyperjeremie@gmail.com']);
-
-// 		    $m->subject('zizi');
-
-// 		    // $m->body('coucou');
-// 		});
-
-// 		// envoi à la personne ayant rempli le formulaire
-// 		Mail::send('email.contact', $inputs, function($m)
-// 		{
-// 		   // $m->from('dossantos.adrien18@gmail.com');
-
-// 		   // $m->to($mail);
-
-// //		    $m->subject($objet);
-
-// //		    $m->body($message);
-// 		});
+		extract($inputs); 
 
 		Mail::send('emails.welcome', array('test' => 'test'), function($message)
         {
             $message->to('clementpeyrabere@gmail.com')->subject('Welcome!');
+            $m->from($mail, $nom." ".$prenom);
+		    $m->to(['dossantos.adrien18@gmail.com', 'clementpeyrabere@gmail.com', 'decuyperjeremie@gmail.com']);
+		    $m->subject('zizi');
+		    $m->body('coucou');
         });
 
 		// si le mail ne s'est pas envoyé
