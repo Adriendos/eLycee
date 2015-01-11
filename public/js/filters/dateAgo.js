590b9932e4a70dfcb8609b5edd6a7b712d3d8df6
de cuyper <decuyperjeremie@gmail.com>
2015-01-11 02:54:20 +0100

    change

--- public/js/filters/dateAgo.js
+++ public/js/filters/dateAgo.js
@@ -1,6 +1,6 @@
 app.filter('dateAgo', function() {
     return function(input) {
         if(!input) return;
-		return moment( new Date(input) ).add(2, 'minutes').utc().fromNow();
+		return moment( new Date(input) ).add(1, 'minutes').utc().fromNow();
     };
 });
