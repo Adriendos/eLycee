6c5648ece803d6ea20640da6312663d975d51f87
de cuyper <decuyperjeremie@gmail.com>
2015-02-06 21:01:25 +0100

    [UPDATE] => test environment

--- public/js/filters/dateAgo.js
+++ public/js/filters/dateAgo.js
@@ -1,6 +1,13 @@
 app.filter('dateAgo', function() {
     return function(input) {
         if(!input) return;
+        var url = window.location.href;
+
+        if( (url.match(/dev/g) || []).length )
+            console.log('dev');
+        else
+            console.log('prod');
+
 		return moment( new Date(input) ).utc().fromNow();
     };
 });
